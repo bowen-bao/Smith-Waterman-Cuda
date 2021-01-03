17b1df58e04dee2223eacd98491547b1d4aeb013
Bowen Bao <bbao@cs.uchicago.edu>
2021-01-02 16:26:53 -0800

    threads

---
+++ thread_vadd.cu
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void add(int *a, int *b, int *c){
+	c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x]; 
+}
+
+
+void random_ints(int *nums, int size){
+	
+	for (int i = 0; i < size; i++){
+		nums[i] = 1;
+	}
+}
+
+
+#define N 512
+int main(void){
+	
+	int *a, *b, *c;			//host copies of a, b, c
+	int *d_a, *d_b, *d_c;	//device copies of a, b, c
+	int size = N * sizeof(int);
+
+	//Allocate space for device copies of a, b, c
+	cudaMalloc((void **) &d_a, size);	//take in address of pointer 
+	cudaMalloc((void **) &d_b, size);
+	cudaMalloc((void **) &d_c, size);
+
+
+	// Alloc space for host copies of a, b, c and setup input values 
+	a = (int *)malloc(size);
+	random_ints(a, N);
+
+	b = (int *)malloc(size);
+	random_ints(b, N); 
+
+	c = (int *)malloc(size);
+
+	//copy inputs to device 
+	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+	//launch add() kernel on GPU with N threads
+	add<<<1, N>>>(d_a, d_b, d_c);
+
+	//copy result back to host 
+	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+	//print results
+	for (int i = 0; i < N; i++){
+		printf("c[%d]=%d\n", i, c[i]);
+	}
+
+	
+	//Cleanup 
+	free(a);
+	free(b);
+	free(c);
+
+	cudaFree(d_a); 
+	cudaFree(d_b);
+	cudaFree(d_c);
+	return 0;
+
+}
